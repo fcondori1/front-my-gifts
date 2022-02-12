c9a4b61ae7b4f769d7648d4118d21d655fcfc56f
Frankz Condori <frankcondori97@gmail.com>
2022-02-12 17:52:45 -0500

    add Router to index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login/Login';
+import Navigation from './components/Navigation/Navigation';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      Hello World from APP!
+      <Navigation />
+      <Login />
     </div>
   );
 }
