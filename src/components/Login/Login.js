753637d507c35111b521162c31c578a7c13035e5
Frankz Condori <frankcondori97@gmail.com>
2022-02-16 23:11:38 -0500

    add signup forms

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Login.module.css';
 
 function Login(props) {
     return (
-        <div>
-            Hello from Login
-        </div>
-    );
+			<>
+				<form className={styles.container}>
+					<h1>Sign-In</h1>
+					<label>Username:</label>
+					<input type='text' placeholder='Enter Username' />
+					<label>Email:</label>
+					<input type='text' placeholder='Enter Email' />
+					<label>Password:</label>
+					<input type='text' placeholder='Enter Password' />
+					<label>Re-enter Password:</label>
+					<input type='text' placeholder='Enter Password' />
+					<button type='submit'>Login</button>
+					<button type='submit'>
+						<Link to='/signup'>sign up</Link>
+					</button>
+				</form>
+			</>
+		);
 }
 
 export default Login;
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Signup.module.css';
 
 function Signup(props) {
-    return (
-        <div>
-            Hello from Signup
-        </div>
-    );
+	return (
+		<>
+			<form className={styles.container}>
+				<h1>Sign-In</h1>
+				<label>Username:</label>
+				<input type='text' placeholder='Enter Username' />
+				<label>Email:</label>
+				<input type='text' placeholder='Enter Email' />
+				<label>Password:</label>
+				<input type='text' placeholder='Enter Password' />
+				<label>Re-enter Password:</label>
+				<input type='text' placeholder='Enter Password' />
+				<button type='submit'>Login</button>
+				<button type='submit'>
+					<Link to='/signup'>sign up</Link>
+				</button>
+			</form>
+		</>
+	);
 }
 
 export default Signup;
